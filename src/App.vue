eff02bfd7a5d27d30c287ea48eb0c8300bc2f91d
saeedmalikx <saeedmalik360@gmail.com>
2017-05-31 17:13:28 -0400

    Added store and implemented axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <nav class="nav has-shadow">
         <div class="container">
             <div class="nav">
@@ -9,6 +10,30 @@
             </div>
         </div>
     </nav>
+    <div class="box">  
+      <div class="card" v-for="post in posts.results">
+          <header class="card-header">
+              <p class="card-header-title">
+              {{post.question}}
+              </p>
+          </header>
+          <div class="card-content">
+              <div class="content">
+              <ul class="menu-list" id="menulist">
+                  <li><a @click="markcorrect()">{{post.correct_answer}}</a></li>
+                  <li><a>{{post.incorrect_answers[0]}}</a></li>     
+                  <li><a>{{post.incorrect_answers[1]}}</a></li>       
+                  <li><a>{{post.incorrect_answers[2]}}</a></li>
+              </ul>
+              </div>
+          </div>
+          <footer class="card-footer">
+              <span class="card-footer-item">Difficulty: {{post.difficulty}}</span>
+              <a @click="getquestion()" class="card-footer-item">Next Question</a>
+          </footer>
+        </div>
+      </div>
+</div>
 </template>
 
 <script>
@@ -16,9 +41,24 @@ export default {
   name: 'app',
   data () {
     return {
-      score: this.$store.state.score
     }
   },
+  methods: {
+      getquestion(){
+        this.$store.commit('getquestion')
+      },
+      markcorrect(){
+        this.$store.dispatch('nextquestion')
+      }
+  },
+  computed: {
+    posts(){
+      return this.$store.state.posts
+    },
+    score(){
+      return this.$store.state.score
+    }
+  }
 }
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 
 Vue.use(Vuex)
 
 
 export default new Vuex.Store({
     state: {
-        score: 3
+        score: 3,
+        posts: []
+    },
+    mutations: {
+        getquestion (state){
+            axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
+        .then(response =>{
+            state.posts = response.data;
+        })},
+        markcorrect (state){
+            state.score++
+        }
+    },
+    actions: {
+        nextquestion({commit}){
+            commit('markcorrect'),
+            commit('getquestion')
+        }
     }
 })
